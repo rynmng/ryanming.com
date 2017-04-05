823e9912884996a7f55bf702d5ab7a80291bbdcb
Ryan Ming <ryan.ming@gmail.com>
2017-04-05 13:14:11 -0400

    reduce canvas size, copy changes

--- main.js
+++ main.js
@@ -2,33 +2,29 @@ const sketchFn = (s) => {
   let yOffset = 0
 
   s.setup = () => {
-    s.createCanvas(s.windowWidth, 300)
+    // s.pixelDensity(s.displayDensity())
+    s.createCanvas(s.windowWidth, 240)
     s.noStroke()
-    s.pixelDensity(s.displayDensity())
   }
 
   s.draw = () => {
-    s.background("#ff4d5d")
+    let xOffset = 0
+
+    // s.background("#ff4d5d")
+    s.clear()
     s.fill("rgba(255, 255, 255, 1)")
 
     s.beginShape()
 
-    let xOffset = 0
-
-    // Iterate over horizontal pixels
     for (let x = 0; x <= s.width; x += 10) {
       let y = s.map(s.noise(xOffset, yOffset), 0, 1, s.height - 100, s.height - 200)
-
-      // Set the vertex
       s.vertex(x, y)
-      // Increment x dimension for noise
       xOffset += 0.05
     }
 
     // patch up to window width
     s.vertex(s.width, s.map(s.noise(xOffset, yOffset), 0, 1, s.height - 100, s.height - 200))
 
-    // increment y dimension for noise
     yOffset += 0.01
     s.vertex(s.width, s.height)
     s.vertex(0, s.height)
@@ -37,7 +33,7 @@ const sketchFn = (s) => {
   }
 
   s.windowResized = () => {
-    s.resizeCanvas(s.windowWidth, 300);
+    s.resizeCanvas(s.windowWidth, 240);
   }
 }
 
