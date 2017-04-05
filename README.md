4c2792c03be411954b8c72a017a17128a2ba2c5e
Ryan Ming <ryan.ming@gmail.com>
2017-04-05 19:43:48 -0400

    some npm scripts to build website into dist

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 [ryanming.com](ryanming.com)
 
-Use `yarn run build` to create a js bundle. I'm only using webpack for js, styles are untouched.
+First `yarn install` to get all the dependencies.
+
+`yarn run build` to create a build of the website in the `dist` folder.
+`yarn run start` run a webserver out of the `dist` folder.
 
--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,13 @@ const webpack = require('webpack')
 module.exports = {
   entry: './js/main.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'dist/js'),
     filename: 'bundle.js'
   },
   module: {
     rules: [
       { test: /gifLinks.js$/, loaders: ["exports-loader?gifLinks=GifLinks"] },
-      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+      { test: /\.js$/, exclude: [/node_modules/, /dist/], loader: "babel-loader" }
     ]
   }
 }
